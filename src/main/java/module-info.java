0b968f7f81adfac0a482cd1d3f6dff489b543dde
leryn <pinktin@sina.com>
2024-04-21 17:49:58 +0800

    feature: great enhancement

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module etcd.jdbc {
+  requires java.sql;
+
+  requires avatica.core;
+  requires calcite.core;
+  requires calcite.linq4j;
+  requires com.fasterxml.jackson.annotation;
+  requires com.fasterxml.jackson.databind;
+  requires com.google.common;
+  requires jetcd.core;
+  requires kubernetes.model.apiextensions;
+  requires kubernetes.model.common;
+  requires kubernetes.model.core;
+  requires static lombok;
+  requires org.slf4j;
+  requires org.jetbrains.annotations;
+
+  exports io.github.leryn.etcd.annotation;
+  exports io.github.leryn.etcd.calcite;
+  exports io.github.leryn.etcd.jdbc;
+  exports io.github.leryn.etcd.kubernetes;
+  exports io.github.leryn.etcd.support;
+}
